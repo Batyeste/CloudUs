dbf36f30d0af656b1faf106f93d270ec779c62d3
Batyeste <batystebatybat@gmail.com>
2024-09-07 18:05:01 +0200

    Ajout etape 4 - Paiement

--- front/src/components/Signup.jsx
+++ front/src/components/Signup.jsx
@@ -14,7 +14,13 @@ const SignUp = () => {
         nomSoce: '',
         adressePostaleSoce: '',
         siret: '',
-        verifCode: ''
+        verifCode: '',
+        tituleCB: '',
+        paymentMethod: '',
+        cardNumber: '',
+        expiryMonth: '',
+        expiryYear: '',
+        cvv: ''
     });
 
     const [step, setStep] = useState(1);
@@ -31,15 +37,35 @@ const SignUp = () => {
                 setSiretError('Le SIRET doit contenir uniquement des chiffres');
             } else if (value.length > 14) {
                 setSiretError('Le SIRET ne doit pas dépasser 14 caractères');
+            } else if (value.length < 14) {
+                setSiretError('Le SIRET doit contenir 14 caractères');
             } else {
                 setSiretError(''); 
             }
         }
 
-        setFormData({
-            ...formData,
-            [name]: value
-        });
+        //-- partie numéro de carte valide
+        if (name === "cardNumber" && value.length <= 16 && /^\d*$/.test(value)) {
+            setFormData({
+                ...formData,
+                [name]: value
+            });
+        }
+
+        //-- partie CVV valide
+        if (name === "cvv" && value.length <= 4 && /^\d*$/.test(value)) {
+            setFormData({
+                ...formData,
+                [name]: value
+            });
+        }
+
+        if (name !== "cardNumber" && name !== "cvv" ) {
+            setFormData({
+                ...formData,
+                [name]: value
+            });
+        }
     };
 
     //-- Validation des étapes - Etape 1 du formulaire
@@ -77,10 +103,28 @@ const SignUp = () => {
         return true;
     };
 
+    //-- Validation des étapes - Etape 4 du formulaire
+    const validateStep4 = () => {
+        if (!formData.tituleCB || !formData.paymentMethod || !formData.cardNumber || !formData.expiryMonth || !formData.expiryYear || !formData.cvv) {
+            setError("Tous les champs sont obligatoires lors de cette étape !");
+            return false;
+        }
+        if (!/^\d{16}$/.test(formData.cardNumber)) {
+            setError("Le numéro de carte doit comporter exactement 16 chiffres !");
+            return false;
+        }
+        if (!/^\d{3,4}$/.test(formData.cvv)) {
+            setError("Le CVV doit comporter 3 à 4 chiffres !");
+            return false;
+        }
+        return true;
+    };
+
 //-- A faire : 
 //-- Si la deuxième étape est valide, envoyer un mail de vérification à l'adresse e-mail fournie
 //-- Donc créer un script qui envoie un mail de vérif avec un code aléatoire qui sera retourner par la fonction et qui sera comparé avec celui écrit
 
+//-- Mettre à l'étape 4 un choix d'abonnement (genre 3 cartes, 9,99, 14,99 et 50,99 euros par mois) et espace de stockage (10, 50, 100 Go)
     const handleSubmit = (e) => {
         e.preventDefault();
         setError('');
@@ -92,12 +136,11 @@ const SignUp = () => {
         } else if (step === 2 && validateStep2()) {
             setStep(3); //-- Passe à l'étape 3 si valide
         } else if (step === 3 && validateStep3()) {
+            setStep(4); //-- Passe à l'étape 4 si valide
+        } else if (step === 4 && validateStep4()) {
             console.log('Form Data:', formData);
-        }
-
-        //-- affichage données du formulaire
-        else if (step === 3) {
-            console.log('Form Data:', formData);
+        } else {
+            console.log('Error:', error);
         }
     };
 
@@ -174,10 +217,63 @@ const SignUp = () => {
                     </div>
                 )}
 
+                {step === 4 && (
+                    <div>
+                        <div className="form-group">
+                            <label htmlFor="tituleCB">Titulaire carte</label>
+                            <input type="text" id="tituleCB" name="tituleCB" value={formData.tituleCB} onChange={handleChange} />
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="paymentMethod">Méthode de paiement</label>
+                            <select id="paymentMethod" name="paymentMethod" value={formData.paymentMethod} onChange={handleChange}>
+                                <option value="">Sélectionnez une méthode</option>
+                                <option value="creditCard">Carte de crédit</option>
+                                <option value="paypal">PayPal</option>
+                                <option value="bankTransfer">Virement bancaire</option>
+                            </select>
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="cardNumber">Numéro de carte</label>
+                            <input type="text" id="cardNumber" name="cardNumber" value={formData.cardNumber} onChange={handleChange} maxLength={16}/>
+                        </div>
+
+                        <div className="form-group">
+                            <label>Date d'expiration</label>
+                            <div>
+                                <select id="expiryMonth" name="expiryMonth" value={formData.expiryMonth} onChange={handleChange} >
+                                    <option value="">Mois</option>
+                                    {[...Array(12)].map((_, i) => (
+                                        <option key={i+1} value={i+1 < 10 ? `0${i+1}` : i+1}>
+                                            {i+1 < 10 ? `0${i+1}` : i+1}
+                                        </option>
+                                    ))}
+                                </select>
+
+                                <select id="expiryYear" name="expiryYear" value={formData.expiryYear} onChange={handleChange} >
+                                    <option value="">Année</option>
+                                    {[...Array(10)].map((_, i) => (
+                                        <option key={i} value={new Date().getFullYear() + i}>
+                                            {new Date().getFullYear() + i}
+                                        </option>
+                                    ))}
+                                </select>
+                            </div>
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="cvv">CVV</label>
+                            <input type="text" id="cvv" name="cvv" value={formData.cvv} onChange={handleChange} maxLength={4}/>
+                        </div>
+                        {error && <p className="error-message">{error}</p>}
+                    </div>
+                )}  
+
                 <div className="form-navigation">
                     {step > 1 && <button type="button" className="submit-button" onClick={handlePrevious}>Précédent</button>}
                     <button type="submit" className="submit-button">
-                        {step === 3 ? "S'inscrire" : "Suivant"}
+                        {step === 4 ? "S'inscrire" : "Suivant"}
                     </button>
                 </div>
             </form>
