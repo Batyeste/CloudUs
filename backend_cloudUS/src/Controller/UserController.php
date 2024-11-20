ea96aab79213de54a42fc64dc35f086e41ed1865
Yohannkp <yoharh56@gmail.com>
2024-11-21 00:21:39 +0100

    Ajout fonctionnalité acheter stockage

--- backend_cloudUS/src/Controller/UserController.php
+++ backend_cloudUS/src/Controller/UserController.php
@@ -35,8 +35,80 @@ class UserController extends AbstractController
     }
 
     #[Route('/api/user/buy-storage', name: 'buy_storage', methods: ['POST'])]
-public function buyStorage(Request $request): JsonResponse
+    public function buyStorage(Request $request): JsonResponse
+    {
+
+
+        $data = json_decode($request->getContent(), true);
+
+            if ($data === null) {
+                return new JsonResponse(['error' => 'Invalid JSON'], JsonResponse::HTTP_BAD_REQUEST);
+            }
+
+            // Accéder aux données du JSON
+            $stockage = $data['stockage'];
+
+        $mail = $this->userRepository->decodeToken($request);
+        $user = $this->userRepository->findOneBy(['email'=> $mail]);
+
+        // Ajouter 20 Go d'espace (20 480 Ko)
+        $additionalStorage = 20480;
+        $additionalStorage = ($additionalStorage * $stockage)/20;
+
+        $user->setTotalStorage($user->getTotalStorage() + $additionalStorage);
+
+    // Sauvegarder la mise à jour de l'utilisateur
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+
+        $invoiceData = [
+            'invoice_number' => uniqid(),
+            'date' => (new \DateTime())->format('Y-m-d'),
+            'client_name' => $user->getNom() . ' ' . $user->getPrenom(),
+            'client_address' => $user->getAdresse(),
+            'total_amount' => $stockage,
+            // Détails de l'achat
+        'description' => 'Achat d\'espace de stockage supplémentaire',
+        'unit_price' => $stockage,    // Prix unitaire hors taxe
+        'quantity' => 1,          // Quantité achetée
+
+        // Montants calculés
+        'total_ht' => 20.00,      // Total hors taxe
+        'tva_rate' => 0.20,       // Taux de TVA (20%)
+        'tva_amount' => 20.00 * 0.20,  // Montant de la TVA
+        'total_ttc' => 20.00 * 1.20    // Montant total toutes taxes comprises
+
+        ];
+        
+        $invoiceService = new InvoiceService();
+        $pdfPath = $invoiceService->generateInvoicePdf($invoiceData);
+        
+        $this->emailService->sendStoragePurchaseNotification($user->getEmail(), $additionalStorage, $pdfPath);
+
+        return new JsonResponse([
+            'status' => 'Espace supplémentaire acheté',
+            'totalStorage' => $user->getTotalStorage(),
+            'usedStorage' => $user->getUserStorage(),
+            'remainingStorage' => $user->getTotalStorage() - $user->getUserStorage()
+        ]);
+    }
+
+#[Route('/api/user/delete-account', name: 'delete_acccount', methods: ['POST'])]
+public function deleteUser(Request $request): JsonResponse
 {
+
+
+    $data = json_decode($request->getContent(), true);
+
+        if ($data === null) {
+            return new JsonResponse(['error' => 'Invalid JSON'], JsonResponse::HTTP_BAD_REQUEST);
+        }
+
+        // Accéder aux données du JSON
+        $stockage = $data['stockage'];
+
+
+
     $user = $this->getUser();
 
     $user = $this->userRepository->findOneBy(["email"=>$user->getUserIdentifier()]);
